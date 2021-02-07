feb615933fcfb1212e43f6e45609cbdb8a54b092
Zxilly <zhouxinyu1001@gmail.com>
2021-02-07 22:02:49 +0800

    better robust

--- api/index.py
+++ api/index.py
@@ -10,11 +10,11 @@ rand = "A41CB32704D5547F3F4C23905FFEDEAB"
 
 def img2base64(inputInfo, rank=False, weapon=False):
     if rank:
-        with open('./page/img/skillgroup' + str(inputInfo) + '.png', 'rb') as f:
+        with open('../page/img/skillgroup' + str(inputInfo) + '.png', 'rb') as f:
             b64data = base64.b64encode(f.read())
             return b64data.decode()
     elif weapon:
-        with open(f'./page/base64/{inputInfo}', 'rb') as f:
+        with open(f'../page/base64/{inputInfo}', 'rb') as f:
             a = f.read()
         return a.decode()
     else:
@@ -57,7 +57,7 @@ def render(player, rankid, svg=False):
                                           player.last_mvp,
                                           player.last_kill,
                                           player.last_death,
-                                          img2base64(player.last_favweapon_id,weapon=True))
+                                          img2base64(player.last_favweapon_id, weapon=True))
     # print(output)
     return output
 
@@ -349,20 +349,21 @@ class Player(object):
         api1_object = requests.get("https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/",
                                    params=api1_info)
         data1 = json.loads(api1_object.content)
-        self.total_kills = data1['playerstats']['stats'][0]['value']
-        self.total_head_kills = data1['playerstats']['stats'][25]['value']
-        self.total_deaths = data1['playerstats']['stats'][1]['value']
-        self.total_wins_round = data1['playerstats']['stats'][5]['value']
-        self.total_round = data1['playerstats']['stats'][46]['value']
-        self.hits = data1['playerstats']['stats'][44]['value']
-        self.fired = data1['playerstats']['stats'][45]['value']
-        self.last_kill = data1['playerstats']['stats'][87]['value']
-        self.last_death = data1['playerstats']['stats'][88]['value']
-        self.last_mvp = data1['playerstats']['stats'][89]['value']
-        self.last_ct = data1['playerstats']['stats'][84]['value']
-        self.last_t = data1['playerstats']['stats'][83]['value']
-        last_win = data1['playerstats']['stats'][85]['value']
-        if (last_win == 15):
+        alist = data1['playerstats']['stats']
+        self.total_kills = deepTraverse(alist, 'total_kills')
+        self.total_head_kills = deepTraverse(alist, 'total_kills_headshot')
+        self.total_deaths = deepTraverse(alist, 'total_wins')
+        self.total_wins_round = deepTraverse(alist, 'total_wins')
+        self.total_round = deepTraverse(alist, 'total_rounds_played')
+        self.hits = deepTraverse(alist, 'last_match_favweapon_hits')
+        self.fired = deepTraverse(alist, 'last_match_favweapon_shots')
+        self.last_kill = deepTraverse(alist, 'last_match_kills')
+        self.last_death = deepTraverse(alist, 'last_match_deaths')
+        self.last_mvp = deepTraverse(alist, 'last_match_mvps')
+        self.last_ct = deepTraverse(alist, 'last_match_ct_wins')
+        self.last_t = deepTraverse(alist, 'last_match_t_wins')
+        last_win = deepTraverse(alist, 'last_match_wins')
+        if last_win == 15:
             self.win_status = '平'
             self.last_t = 15
             self.last_ct = 15
@@ -379,9 +380,9 @@ class Player(object):
                 self.win_status = '胜'
             else:
                 self.win_status = '负'
-        self.last_favweapon_id = data1['playerstats']['stats'][90]['value']
-        self.last_favweapon_shots = data1['playerstats']['stats'][91]['value']
-        self.last_favweapon_hits = data1['playerstats']['stats'][92]['value']
+        self.last_favweapon_id = deepTraverse(alist, 'last_match_favweapon_id')
+        self.last_favweapon_shots = deepTraverse(alist, 'last_match_favweapon_shots')
+        self.last_favweapon_hits = deepTraverse(alist, 'last_match_favweapon_hits')
 
         # num = 0
         # for one in data1['playerstats']['stats']:
@@ -397,6 +398,13 @@ class Player(object):
         self.player_name = data2['response']['players'][0]['personaname']
         self.avatar_url = data2['response']['players'][0]['avatarfull']
         self.logoff_time = data2['response']['players'][0]['lastlogoff']
+
+
+def deepTraverse(alist, key):
+    for one in alist:
+        if one['name'] == key:
+            return one['value']
+    raise Exception("Not Found")
 
 
 def cache(player, rankid):
